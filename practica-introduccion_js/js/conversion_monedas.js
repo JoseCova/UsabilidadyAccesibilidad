2a8252e5e944347e8990dcf48f2d4d3ce85e5de8
alu0101021768 <alu0101021768@ull.edu.es>
2020-03-27 18:55:26 +0000

    Conversor de monedas finalizado

--- practica-introduccion_js/js/conversion_monedas.js
+++ practica-introduccion_js/js/conversion_monedas.js
@@ -1,61 +1,53 @@
 "use strict";
 
-const moneda = document.getElementById("moneda").value;
-const valor = document.getElementById("cantidad").value;
+const moneda = document.getElementById("moneda");
+const valor = document.getElementById("cantidad");
+const parrafo = document.createElement("pre");
+let text;
 
 function conversion() {
-    if (moneda === "eur") {
-        console.log(valor + " euros = " + (valor * 1.09).toFixed(3) + "dolares");
-        console.log(valor + "euros = " + (valor * 69.6).toFixed(3) + "pesos argentinos");
-        console.log(valor + " euros = " + (valor * 121.0).toFixed(3) + " yenes");
-    } else if (moneda === "yen") {
-        console.log(valor + " yenes = " + (valor * 0.0083).toFixed(3) + " euros");
-        console.log(valor + " yenes = " + (valor * 0.009).toFixed(3) + "dolares");
-        console.log(valor + " yenes = " + (valor * 0.58).toFixed(3) + " pesos argentinos");
-    } else if (moneda === "dollar") {
-        console.log(valor + " dolares = " + (valor * 0.92).toFixed(3) + " euros");
-        console.log(valor + " dolares = " + (valor * 0.92).toFixed(3) + " euros");
-        console.log(valor + " dolares = " + (valor * 0.92).toFixed(3) + " euros");
-    } else if (moneda === "pesos") {
-        console.log(valor + " pesos argentinos = " + (valor * 0.014).toFixed(3) + " euros");
-        console.log(valor + " pesos argentinos = " + (valor * 0.016).toFixed(3) + " dolares");
-        console.log(valor + " pesos argentinos = " + (valor * 1.74).toFixed(3) + " yenes");
+    if (moneda.value == "eur") {
+        text = `
+        ${valor.value} euros equivalen a ${(valor.value * 1.09).toFixed(3)} dolares
+        ${valor.value} euros equivalen a ${(valor.value * 69.6).toFixed(3)} pesos argentinos
+        ${valor.value} euros equivalen a ${(valor.value * 121.0).toFixed(3)} yenes
+        `;
+        parrafo.textContent = text;
+        document.body.appendChild(parrafo);
+
+    } else if (moneda.value == "yen") {
+        text = `
+        ${valor.value} yenes equivalen a ${(valor.value * 0.0083).toFixed(3)} euros
+        ${valor.value} yenes equivalen a ${(valor.value * 0.009).toFixed(3)} dólares
+        ${valor.value} yenes equivalen a ${(valor.value * 0.58).toFixed(3)} peso argentino
+		`;
+        parrafo.textContent = text;
+        document.body.appendChild(parrafo);
+    } else if (moneda.value == "dollar") {
+        text = `
+        ${valor.value} dólares equivalen a ${(valor.value * 0.92).toFixed(3)} euros
+        ${valor.value} dólares equivalen a ${(valor.value * 111.31).toFixed(3)} yenes
+        ${valor.value} dólares equivalen a ${(valor.value * 64.06).toFixed(3)} peso argentino
+		`;
+        parrafo.textContent = text;
+        document.body.appendChild(parrafo);
+    } else if (moneda.value == "pesos") {
+        text = `
+        ${valor.value} pesos argentinos equivalen a ${(valor.value * 0.014).toFixed(3)} euros
+        ${valor.value} pesos argentinos equivalen a ${(valor.value * 1.74).toFixed(3)} yenes
+        ${valor.value} pesos argentinos equivalen a ${(valor.value * 0.016).toFixed(3)} dólares
+		`;
+        parrafo.textContent = text;
+        document.body.appendChild(parrafo);
+    } else {
+        text = `
+		Introduzca un tipo de moneda que esté soportado...
+		eur para Euros
+		dollar para US Dollars
+		yen para Yenes Japoneses
+		pesos para Pesos Argentinos
+		`;
+        parrafo.textContent = text;
+        document.body.appendChild(parrafo);
     }
 }
-
-/*
-var valor = 420
-
-function toEuros() {
-    console.log(valor + ' dolares = ' + (valor * 0.92).toFixed(3) + ' euros')
-    console.log(valor + ' yenes = ' + (valor * 0.0083).toFixed(3) + ' euros')
-    console.log(valor + ' pesos argentinos = ' + (valor * 0.014).toFixed(3) + ' euros')
-}
-
-function toDollars() {
-    
-    console.log(valor + ' yenes = ' + (valor * 0.0090).toFixed(3) + 'dolares')
-    console.log(valor + ' pesos argentinos = ' + (valor * 0.016).toFixed(3) + ' dolares')
-}
-
-function toPesosArgentinos() {
-    console.log('${valor.}  euros = (valor * 69.60).toFixed(3) pesos argentinos')
-    console.log(valor + ' yenes = ' + (valor * 0.58).toFixed(3) + ' pesos argentinos')
-    console.log(valor + ' dolares = ' + (valor * 0.92).toFixed(3) + ' euros')
-}
-
-function toYenes() {
-    console.log(valor + ' euros = ' + (valor * 121.00).toFixed(3) + ' yenes')
-    console.log(valor + ' pesos argentinos = ' + (valor * 1.74).toFixed(3) + ' yenes')
-    console.log(valor + ' dolares = ' + (valor * 0.92).toFixed(3) + ' euros')
-}
-
-console.log('Conversión de varias monedas a euros')
-toEuros()
-console.log('Conversión de varias monedas a dólares estadounidenses')
-toDollars()
-console.log('Conversión de varias monedas a pesos argentinos')
-toPesosArgentinos()
-console.log('Conversión de varias monedas a yenes')
-toYenes()
-*/
