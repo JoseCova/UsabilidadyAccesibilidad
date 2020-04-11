61c4ae34ed1f5bbb18ba72ccf295c4bb2db541ff
JoseCova <alu0101023663@ull.edu.es>
2020-04-11 21:10:10 +0100

    Añadido recoger postID al que hacer la peticion

--- practica-introduccion_ajax/ajax.js
+++ practica-introduccion_ajax/ajax.js
@@ -1,13 +1,14 @@
 
 $("#boton").click(function getDatos(){
+    let postiD = $("#zonatexto").val();
     $.ajax({
-        url: 'https://jsonplaceholder.typicode.com/comments?postId=1',
+        url: `https://jsonplaceholder.typicode.com/comments?postId=${postiD}`,
         type: 'GET',
         async: true,
 
         success: function(resultado) {
 
-        var datos = $("#parrafo");
+        let datos = $("#parrafo");
             $.each(resultado, function(index, value) {
             datos.append(
                 `<p>PostId: ${value.postId} || id: ${value.id} || Nombre: ${value.name} || Email: ${value.email} || Body: ${value.body} </p>`
