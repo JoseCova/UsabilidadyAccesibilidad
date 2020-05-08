59e3d849fc56fe38614df158f8e02ad2c8568315
JoseCova <alu0101023663@ull.edu.es>
2020-05-08 04:56:20 +0100

    Creado todo el setup para que la pagina se pueda desplegar y aparezca en internet usando firebase hosting

---
+++ proyecto-final/public/js/init.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+  apiKey: "AIzaSyB_ejoYllafjSBBE3XeQIT9BarCFEijCsQ",
+  authDomain: "uya-proyecto-final.firebaseapp.com",
+  databaseURL: "https://uya-proyecto-final.firebaseio.com",
+  projectId: "uya-proyecto-final",
+  storageBucket: "uya-proyecto-final.appspot.com",
+  messagingSenderId: "1030376776101",
+  appId: "1:1030376776101:web:511c39ddc9c3f3b3a8c84c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
